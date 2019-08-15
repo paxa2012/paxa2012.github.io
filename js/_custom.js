df22d46278da6c02d016c6f77368424fb9e99b94
paxa2012 <32680173+paxa2012@users.noreply.github.com>
2019-08-15 10:06:11 +0700

    123

---
+++ js/_custom.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+	// Custom JS
+	$('.filters li').click(function() {			
+		$('.filters li').removeClass('active');
+		$(this).addClass('active');
+	
+	});
+
+	
+});
