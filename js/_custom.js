e5e9c26b5dfee8d26060c1ca3c20f4c6715e26fc
paxa2012 <32680173+paxa2012@users.noreply.github.com>
2019-08-16 16:10:06 +0700

    123

--- js/_custom.js
+++ js/_custom.js
@@ -7,9 +7,14 @@ document.addEventListener("DOMContentLoaded", function() {
 	
 	});
 
+	$('.carousel-indicators-my li').click(function(){
+		$('.carousel-indicators-my li').removeClass('active');
+		$(this).addClass('active');
+	});
+
 	$(window).on("scroll", function() {
         $(document).scrollTop() > 840 ? $(".navbar").addClass("fixed-top") : $(".navbar").removeClass("fixed-top")
 	})
 	
-	
+
 });
