154fbd9bcf51bc91edd3c99444253c9cd9f87cc1
paxa2012 <32680173+paxa2012@users.noreply.github.com>
2019-08-20 20:01:27 +0700

    123

---
+++ js/common.js
@@ -0,0 +1,90 @@
+$(function() {
+
+	// Custom JS
+	$('.filters li').click(function () {
+		$('.filters li').removeClass('active');
+		$(this).addClass('active');
+
+	});
+
+	$('.carousel-indicators-my li').click(function () {
+		$('.carousel-indicators-my li').removeClass('active');
+		$(this).addClass('active');
+	});
+	$('.showw').css({'display': 'none'});
+	$('.navbar-toggler').click(function () {
+		$('.collapse').addClass('showw');
+		//alert(123);
+		$('.showw').slideToggle('slow', function(){
+			if($('.showw').css('display') == 'none'){
+				$('.collapse').removeClass('showw');
+			}
+		});
+
+	});
+	$(window).on("scroll", function () {
+		$(document).scrollTop() > 840 ? $(".navbar").addClass("fixed-top") : $(".navbar").removeClass("fixed-top")
+	});
+	$(window).on("scroll", function () {
+		$(document).scrollTop() > 1840 ? $(".button-up").css({'display': 'block'}) : $(".button-up").css({'display': 'none'})
+	});
+
+	$('.nav-link').on('click', function(e){
+        e.preventDefault();
+	});
+
+	$('.button-up').on("click", function() {
+        $("html, body").animate({
+			scrollTop: 0
+		}, 1000);
+		
+           
+	});
+
+	$('.gallery').mauGallery({
+		columns: {
+			xs: 1,
+			sm: 2,
+			md: 3,
+			lg: 4,
+			xl: 6
+		},
+		//lightBox: true,
+		//lightboxId: 'myAwesomeLightbox',
+		showTags: false,
+		tagsPosition: 'top'
+	});
+
+	$('.counter').counterUp({
+		delay: 10,
+		time: 1000
+	});
+
+	  $(".link-nav").click(function () {
+		$(".link-nav").removeClass('activ');
+		$(this).addClass('activ')
+
+		$("html, body").animate({
+			scrollTop: $($(this).attr("href")).offset().top + margin + "px" // .top+margin - ставьте минус, если хотите увеличить отступ
+		}, {
+				duration: 1000, // тут можно контролировать скорость
+				easing: "swing"
+			});
+
+		return false;
+	});
+
+	
+
+});
+
+var vid = document.getElementById('my-video');
+function playVid(){
+	return vid.paused ? vid.play(
+		$('.heder-img-video .fas').removeClass('fa-play'),
+		$('.heder-img-video .fas').addClass('fa-pause'),
+	) : vid.pause(
+		$('.heder-img-video .fas').removeClass('fa-pause'),
+		$('.heder-img-video .fas').addClass('fa-play'),
+	);
+}
