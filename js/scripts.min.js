a17ae557fb36a866baad1ebcff29a5452cbd9719
paxa2012 <32680173+paxa2012@users.noreply.github.com>
2019-09-19 17:41:20 +0700

    123

--- js/scripts.min.js
+++ js/scripts.min.js
@@ -1,60 +1,67 @@
-$(function() {
+$(function () {
 
-	// Custom JS
-	
-		$('.box-item-body').hide();
-	$('.box-item').on('click', function(){
-		$('.box-item').removeClass('active');
-		$(this).addClass('active');
-		if($('.box-item').hasClass('active')){
+	/// аккордион меню
+	/*$('.box-item-body').hide();
+	$('.box-item').on('click', function () {
+		if ($('.box-item').hasClass('active')) {
 			$('.box-item-body').slideUp();
 			$(this).find('.box-item-body').slideDown();
 		}
-		else{
+		else {
 			$('.box-item-body').slideUp();
 		}
+	});*/
+
+
+	//// выбирает активный слад в карусели
+	$('.carousel-indicators-my li').click(function () {
+		$('.carousel-indicators-my li').removeClass('active');
+		$(this).addClass('active');
 	});
 
-	
+
+
+	///мобильная навигация открывает и закрываетменю
+	$('.showw').css({ 'display': 'none' });
+	$('.navbar-toggler').click(function () {
+		$('.collapse').addClass('showw');
+		//alert(123);
+		$('.showw').slideToggle('slow', function () {
+			if ($('.showw').css('display') == 'none') {
+				$('.collapse').removeClass('showw');
+			}
+		});
+
+	});
+
+
+
+
+	/// прилипани меню к верху экрана
+	$(window).on("scroll", function () {
+		$(document).scrollTop() > 840 ? $(".navbar").addClass("fixed-top") : $(".navbar").removeClass("fixed-top");
+
+	});
+
+
+	/// отступ первой секции от меню
+	$(window).on("scroll", function () {
+		$(document).scrollTop() > 840 ? $(".block-services").css({ 'margin-top': '217px' }) : $(".block-services").css({ 'margin-top': '81px' });
+	});
+
+
+
+
+	//////// добовляет сласс active для секции work
+
 	$('.filters li').click(function () {
 		$('.filters li').removeClass('active');
 		$(this).addClass('active');
 
 	});
 
-	$('.carousel-indicators-my li').click(function () {
-		$('.carousel-indicators-my li').removeClass('active');
-		$(this).addClass('active');
-	});
-	$('.showw').css({'display': 'none'});
-	$('.navbar-toggler').click(function () {
-		$('.collapse').addClass('showw');
-		//alert(123);
-		$('.showw').slideToggle('slow', function(){
-			if($('.showw').css('display') == 'none'){
-				$('.collapse').removeClass('showw');
-			}
-		});
-
-	});
-	$(window).on("scroll", function () {
-		$(document).scrollTop() > 840 ? $(".navbar").addClass("fixed-top") : $(".navbar").removeClass("fixed-top");
-		$(document).scrollTop() > 840 ? $(".block-services").css({'margin-top': '217px'}) : $(".block-services").css({'margin-top': '81px'});
-	});
-	$(window).on("scroll", function () {
-		$(document).scrollTop() > 1840 ? $(".button-up").css({'display': 'block'}) : $(".button-up").css({'display': 'none'})
-	});
-
-	$('.nav-link').on('click', function(e){
-        e.preventDefault();
-	});
-
-	$('.button-up').on("click", function() {
-        $("html, body").animate({
-			scrollTop: 0
-		}, 1000);
-		
-           
+	$('.nav-link').on('click', function (e) {
+		e.preventDefault();
 	});
 
 	$('.gallery').mauGallery({
@@ -65,38 +72,51 @@ $(function() {
 			lg: 4,
 			xl: 6
 		},
-		//lightBox: true,
-		//lightboxId: 'myAwesomeLightbox',
 		showTags: false,
 		tagsPosition: 'top'
 	});
 
+
+
+
+	//// плавающая кнопка поднимает до начало документа
+	$(window).on("scroll", function () {
+		$(document).scrollTop() > 1840 ? $(".button-up").css({ 'display': 'block' }) : $(".button-up").css({ 'display': 'none' })
+	});
+
+	$('.button-up').on("click", function () {
+		$("html, body").animate({
+			scrollTop: 0
+		}, 1000);
+	});
+
+
+	//////// counter для секции overlay
 	$('.counter').counterUp({
 		delay: 10,
 		time: 1000
 	});
 
-	  $(".link-nav").click(function () {
+
+	///// плавная прокрутка при нажатии на пункты меню
+	$(".link-nav").click(function () {
 		$(".link-nav").removeClass('activ');
 		$(this).addClass('activ')
 
 		$("html, body").animate({
 			scrollTop: $($(this).attr("href")).offset().top // .top+margin - ставьте минус, если хотите увеличить отступ
 		}, {
-				duration: 1000, // тут можно контролировать скорость
-				easing: "swing"
-			});
+			duration: 1000, // тут можно контролировать скорость
+			easing: "swing"
+		});
 
 		return false;
 	});
-
-	
-
 });
 
-
+/// плэй стоп видео в шапке сайта
 var vid = document.getElementById('my-video');
-function playVid(){
+function playVid() {
 	return vid.paused ? vid.play(
 		$('.header-img-video .far').removeClass('fa-play-circle'),
 		$('.header-img-video .far').addClass('fa-pause-circle'),
