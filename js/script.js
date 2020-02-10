0db21d5e300e44ec6d550de930f89cf2b7753db3
paxa2012 <32680173+paxa2012@users.noreply.github.com>
2020-02-11 00:21:17 +0700

    версия 1.1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-$(function(){
+$(function () {
 
+    $('.menu').on('click', function () {
+        $('.toolbar').css('margin-left', '0')
+        $('.menu').css('display', 'none')
+        $('.close').css('display', 'block')
+    })
+    $('.close').on('click', function () {
+        $('.toolbar').css('margin-left', '-13%')
+        $('.close').css('display', 'none')
+        $('.menu').css('display', 'block')
+    })
 
-    
 });
