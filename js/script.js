99ae803315e29ede4dcaac74d4fab85e1642bcf9
paxa2012 <32680173+paxa2012@users.noreply.github.com>
2020-03-02 14:52:10 +0700

    Дороботки по сайту
    перенастройки стилей

--- js/script.js
+++ js/script.js
@@ -1,18 +1,8 @@
 $(function () {
 
 
-    //Меню открыть/закрыть
-    $('.menu').on('click', function () {
-        $('.toolbar').css('margin-left', '0')
-        $('.menu').css('display', 'none')
-        $('.close').css('display', 'block')
-    })
-    $('.close').on('click', function () {
-        $('.toolbar').css('margin-left', '-13%')
-        $('.close').css('display', 'none')
-        $('.menu').css('display', 'block')
-    })
 
-    
+ 
+    $('.sidenav').sidenav();
 
 });
