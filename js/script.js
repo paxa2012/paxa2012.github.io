9cd0a1392eca333a10c2224338bc1649576cd2e6
paxa2012 <32680173+paxa2012@users.noreply.github.com>
2020-03-21 23:47:13 +0700

    добовления в файл Азотные минеральные удобрения

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 $(function () {
 
-
+$('.sidenav-trigger').on('click', function(){
+     $('.toolbar').css({
+        'transform': 'translateX(0)'
+     })
+})
+$('.close').on('click', function(){
+    $('.toolbar').css({
+        'transform': 'translateX(-350px)'
+     })
+})
 
  
     //$('.sidenav').sidenav();
