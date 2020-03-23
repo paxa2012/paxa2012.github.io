adb8f8fd3e8c4dff7ce4fe49154a2c34a11242ae
paxa2012 <32680173+paxa2012@users.noreply.github.com>
2020-03-23 20:53:48 +0700

    123

--- js/script.js
+++ js/script.js
@@ -4,6 +4,16 @@ $('.sidenav-trigger').on('click', function(){
      $('.toolbar').css({
         'transform': 'translateX(0)'
      })
+     /*$('.content').css({
+         'position': 'absolute',
+         'top': '0',
+         'left': '0',
+         'right': '0',
+         'bottom': '0',
+         'opacity': '0.92',
+         'background-color': '#aaa',
+         
+     })*/
 })
 $('.close').on('click', function(){
     $('.toolbar').css({
@@ -11,6 +21,28 @@ $('.close').on('click', function(){
      })
 })
 
+$('.readmore_start').on('click', function(){
+    $('.readmore').css({
+        'display': 'block'
+    })
+    $('.readmore_start').css({
+        'display': 'none'
+    })
+    $('.readmore_end').css({
+        'display': 'inline-block'
+    })
+})
+$('.readmore_end').on('click', function(){
+    $('.readmore').css({
+        'display': 'none'
+    })
+    $('.readmore_start').css({
+        'display': 'inline-block'
+    })
+    $('.readmore_end').css({
+        'display': 'none'
+    })
+})
  
     //$('.sidenav').sidenav();
 
